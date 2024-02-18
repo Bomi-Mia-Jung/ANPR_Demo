97a94840724bb64ec3f800cc6fa487af983a1951
Soyon <sochoi25@amherst.edu>
2024-02-17 22:50:16 -0500

    finished interactive demo -- a little clunky, the drags and updates could be smoother/more frequent

--- lwlr.py
+++ lwlr.py
@@ -6,30 +6,46 @@ import matplotlib.pyplot as plt
 
 
 class LWLR(nn.Module):
-    def __init__(self, d, kernel):
+    def __init__(self, d, kernel, bandwidth):
         super(LWLR, self).__init__()
         self.d = d
         self.theta = np.zeros((d, 1))
         self.kernel = kernel
+        self.bandwidth = bandwidth
 
     def forward(self, x_input, X, Y):
         X_bias = np.ones((X.shape[0], 1))
         X = np.concatenate([X_bias, X], axis=1)
 
-        kern_fn = kernel(mu=x_input, sigma=4)
         x_input = np.array([[x_input]])
         x_bias = np.ones((1, 1))
         x_input = np.concatenate([x_bias, x_input], axis=1)
         # print('x_input shape after bias concat: ', x_input.shape)
 
-        W = kern_fn.get_weights(x_input, X)
+        W = self.get_weights(x_input, X)
         self.fit(X, Y, W)
         return self.theta.T @ x_input.T
 
-    def get_curr_tangent_line(self, domain):
-        return [self.theta.T @ np.concatenate([np.ones((1, 1)), np.array([[x]])], axis=1).T for x in domain]
+    def get_weights(self, x_input, X):
+        kern_fn = self.kernel(mu=x_input, sigma=self.bandwidth)
+        W = kern_fn.get_weights(x_input, X)
+        return W
 
-    def get_curve(self, domain):
+    def get_local_line(self, x_input, X, Y, domain):
+        X_bias = np.ones((X.shape[0], 1))
+        X = np.concatenate([X_bias, X], axis=1)
+
+        x_input = np.array([[x_input]])
+        x_bias = np.ones((1, 1))
+        x_input = np.concatenate([x_bias, x_input], axis=1)
+        # print('x_input shape after bias concat: ', x_input.shape)
+
+        W = self.get_weights(x_input, X)
+        theta = np.linalg.inv(X.T@W@X)@(X.T@W@Y)
+        # print(theta)
+        return [theta.T @ np.concatenate([np.ones((1, 1)), np.array([[x]])], axis=1).T for x in domain]
+
+    def get_curve(self, domain, X, Y):
         return [self.forward(x, X, Y) for x in domain]
 
     def fit(self, X, Y, W):
@@ -38,6 +54,7 @@ class LWLR(nn.Module):
         # print(Y.shape)
         theta = np.linalg.inv(X.T@W@X)@(X.T@W@Y)
         self.theta = theta
+        return self.theta
 
 
 class GaussianKernel:
@@ -71,28 +88,30 @@ if __name__ == '__main__':
     y_outputs = []
 
     kernel = GaussianKernel
-    model = LWLR(d, kernel)
+    model = LWLR(d, kernel, 2)
 
     for x_input in x_inputs:
-        print("input: ", x_input)
+        # print("input: ", x_input)
         y_output = model(x_input, X, Y)
-        print("output: ", y_output.item())
+        # print("output: ", y_output.item())
         y_outputs.append(y_output.item())
         x_plot = np.linspace(0, 35, 30)
 
-        # plot the tangent line being learned at the test x_input
-        y_plot = np.array(model.get_curr_tangent_line(x_plot)).squeeze()
-        plt.plot(x_plot, y_plot, '-')
+        # plot the local line being learned at the test x_input
+        y_plot = np.array(model.get_local_line(x_input, X, Y, x_plot)).squeeze()
+        plt.plot(x_plot, y_plot, '-', label=('locally learned line at point {}'.format(x_input)))
 
     plt.plot(x_inputs, y_outputs, 'x')
 
     # plot curve
     x_plot = np.linspace(0, 35, 50)
-    y_plot = np.array(model.get_curve(x_plot)).squeeze()
+    y_plot = np.array(model.get_curve(x_plot, X, Y)).squeeze()
     plt.plot(x_plot, y_plot, '--')
 
     plt.title('Locally Weighted Linear Regression')
     plt.xlabel('x')
+    plt.xlim((0, 30))
     plt.ylabel('y')
     plt.ylim((0, 20))
+    plt.legend(loc='best')
     plt.show()
