89db29bb7c02cd4f46f012a5c137a2b1a1f2627d
Soyon <sochoi25@amherst.edu>
2024-02-23 01:43:15 -0500

    add limit r to attack

--- attack.py
+++ attack.py
@@ -1,12 +1,11 @@
-import autograd.numpy as np
+import numpy as np
 from lwlr import LWLR, GaussianKernel
 import autograd as ad
 
 class TrTimeAttackOnX:
-    def __init__(self, X, Y, r, learner, lr=0.01):
+    def __init__(self, X, Y, r, learner):
         self.init_X, self.init_Y = X, Y  # initial data
         self.r = r  # limit on how much the attacker can change
-        self.lr = lr
         self.learner = learner  # the learner to attack
         self.delta = np.random.rand(X.shape[0], X.shape[1])
 
@@ -14,16 +13,18 @@ class TrTimeAttackOnX:
         theta_x = self.learner.forward(x_target, self.init_X+delta, self.init_Y+self.delta)
         return (1.0/2)*(theta_x-y_target)**2
 
-    def fit(self, x_target, y_target, epochs):
+    def fit(self, x_target, y_target, lr, epochs):
         # trains the model for self.epochs number of epochs on the entire training set
 
         for epoch in range(epochs):
             grad_fn = ad.grad(self.loss, 2)
             grad = grad_fn(x_target, y_target, self.delta)
-            self.delta = self.delta - grad * self.lr
-
             pred = self.learner.forward(x_target, self.init_X+self.delta, self.init_Y)
             print('Epoch %d/%d, Target y: %.4f, Current prediction: %.4f' % (epoch, epochs, y_target, pred))
+
+            if np.linalg.norm(self.delta - grad * lr) > r:
+                break
+            self.delta = self.delta - grad * lr
 
 
 if __name__ == '__main__':
@@ -37,8 +38,8 @@ if __name__ == '__main__':
     x_target, y_target = 2., 10.
 
     model = LWLR(d=1, kernel=GaussianKernel, bandwidth=2, lbda=0)
-    adversary = TrTimeAttackOnX(X, Y, 5, model)
-    adversary.fit(x_target, y_target, epochs=50)
+    adversary = TrTimeAttackOnX(X, Y, 2, model)
+    adversary.fit(x_target, y_target, lr=0.1, epochs=100)
     print("delta: ", adversary.delta)
     print("target y: ", y_target)
     print("model prediction at x: ", model.forward(x_target, X+adversary.delta, Y))
