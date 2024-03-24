dfd8d6b012c846cc871fdd04734b239fb4688980
Soyon <sochoi25@amherst.edu>
2024-03-24 10:49:06 -0400

    added repulsive attack

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ from matplotlib.backend_bases import MouseEvent
 # import personal modules
 from lwlr import LWLR, EpanechnikovKernel, TricubeKernel, LaplacianKernel, GaussianKernel, UniformKernel, AnovaRBFKernel, TriangularKernel
 from draggable import DraggablePlotTr, DraggablePlotTe
-from attack import TrTimeAttackOnX
+from attack import AttractiveAttackOnX
 
 
 if __name__ == '__main__':
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     X = np.reshape(X, (X.size, 1))  # (n, d)
     Y = np.array(Y)
     Y = np.reshape(Y, (Y.size, 1))  # (n, 1)
-    adversary = TrTimeAttackOnX(X, Y, r, model, lr=0.1, epochs=100)
+    adversary = AttractiveAttackOnX(X, Y, r, model, lr=0.1, epochs=100)
 
     te_plot = DraggablePlotTe(points=data, test_points=[25], r=r, domain=x_range, range=y_range, title="ANPR Draggable Target", model=model, attack=adversary)
